e9685b4ac6f1a8f78e99b64de32b5f83d629d210
TeawoKim <tkim52@my.centennialcollege.ca>
2025-03-21 01:53:57 -0400

    app.py

--- backend/app.py
+++ backend/app.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 import cx_Oracle
 import sys
 import os
+from datetime import datetime
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
+current_dir = os.path.dirname(os.path.abspath(__file__))    # set current directory in order to import config.py
 sys.path.append(os.path.dirname(current_dir))
 from config import get_db_connection
 app = Flask(__name__)
@@ -12,22 +13,34 @@ app = Flask(__name__)
 @app.route('/hire_staff', methods=['POST'])
 def hire_staff():
     if request.method == 'POST':
-        staff_no = request.form['staff_no']
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        position = request.form['position']
-        sex = request.form['sex']
-        dob = request.form['dob']
-        salary = request.form['salary']
-        branch_no = request.form['branch_no']
-        telephone = request.form['telephone']
+        staff_no = request.form.get('staff_no', '').strip()
+        first_name = request.form.get('first_name', '').strip()
+        last_name = request.form.get('last_name', '').strip()
+        position = request.form.get('position', '').strip()
+        sex = request.form.get('sex', '').strip()
+        branch_no = request.form.get('branch_no', '').strip()
+        telephone = request.form.get('telephone', '').strip()
+        mobile = request.form.get('mobile', '').strip()
+        email = request.form.get('email', '').strip()
+        # Make sure user type staff_no
+        if not staff_no:
+            flash("You must type Staff Number.", "danger")
+            return redirect(url_for('index'))
+        # Convert dob string to datetime format
+        dob_str = request.form.get('dob', '').strip()
+        dob = datetime.strptime(dob_str, '%Y-%m-%d') if dob_str else None
+        # Convert salary string to int format
+        salary = request.form.get('salary', '').strip()
+        salary = int(salary) if salary.isdigit() else 0
 
+        connection = None
+        cursor = None
         try:
             connection = get_db_connection()    # Connection Oracle DB
             cursor = connection.cursor()
 
             cursor.callproc("staff_hire_sp", [
-                staff_no, first_name, last_name, position, sex, dob, salary, branch_no, telephone
+                staff_no, first_name, last_name, position, sex, dob, salary, branch_no, telephone,
             ])
 
             connection.commit()
@@ -45,5 +58,5 @@ def index():
     return render_template('hire_staff.html')
 
 if __name__ == '__main__':
-    app.secret_key = 'your_secret_key'  # Flash 메시지를 사용하기 위한 Secret Key
+    app.secret_key = 'your_secret_key'
     app.run(debug=True)
